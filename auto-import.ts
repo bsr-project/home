15a27ed0e6bad5b6a363640462c11da708518c48
imba97 <mail@imba97.cn>
2023-08-16 23:55:14 +0800

    feat: 自动引入

---
+++ auto-import.ts
@@ -0,0 +1,55 @@
+export default [
+  'vue',
+  'vue-router',
+  'pinia',
+  {
+    lodash: [
+      ['get', '_get'],
+      ['has', '_has'],
+
+      ['trim', '_trim'],
+      ['last', '_last'],
+      ['find', '_find'],
+      ['some', '_some'],
+      ['slice', '_slice'],
+      ['chunk', '_chunk'],
+      ['times', '_times'],
+      ['split', '_split'],
+      ['uniqBy', '_uniqBy'],
+      ['sortBy', '_sortBy'],
+      ['random', '_random'],
+      ['assign', '_assign'],
+      ['concat', '_concat'],
+      ['uniqueId', '_uniqueId'],
+      ['findIndex', '_findIndex'],
+
+      ['map', '_map'],
+      ['keys', '_keys'],
+      ['values', '_values'],
+      ['pullAt', '_pullAt'],
+      ['remove', '_remove'],
+      ['filter', '_filter'],
+      ['reduce', '_reduce'],
+      ['flatten', '_flatten'],
+      ['indexOf', '_indexOf'],
+      ['forEach', '_forEach'],
+      ['cloneDeep', '_cloneDeep'],
+      ['debounce', '_debounce'],
+
+      ['startsWith', '_startsWith'],
+      ['endsWith', '_endsWith'],
+
+      ['parseInt', '_parseInt'],
+
+      ['isNull', '_isNull'],
+      ['isEmpty', '_isEmpty'],
+      ['isArray', '_isArray'],
+      ['isObject', '_isObject'],
+      ['isString', '_isString'],
+      ['isNumber', '_isNumber'],
+      ['isUndefined', '_isUndefined'],
+      ['isFunction', '_isFunction'],
+      ['includes', '_includes']
+    ]
+  }
+]
--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,37 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import UnoCSS from 'unocss/vite'
 import { presetWind } from 'unocss'
 
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+import autoImportData from './auto-import'
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  assetsInclude: ['**/*.docx'],
   plugins: [
     vue(),
     vueJsx(),
+
+    AutoImport({
+      imports: autoImportData as any[],
+      dts: fileURLToPath(
+        new URL('./src/types/auto-import.d.ts', import.meta.url)
+      ),
+      eslintrc: {
+        enabled: true
+      },
+      resolvers: [ElementPlusResolver()]
+    }),
+
+    Components({
+      dts: fileURLToPath(
+        new URL('./src/types/components.d.ts', import.meta.url)
+      ),
+      resolvers: [ElementPlusResolver()]
+    }),
+
     UnoCSS({
       presets: [presetWind()]
     })
